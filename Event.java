bb36018db41542cddf64c0d881808582fa329669
AnAccountForSchool <166833777+AnAccountForSchool@users.noreply.github.com>
2024-04-17 10:06:38 -0500

    Update Event.java
    Added more functionality, changed constructor, added fromICS()

--- Event.java
+++ Event.java
@@ -4,36 +4,56 @@
  */
 package cms;
 
+import java.util.ArrayList;
 import java.time.Month;
+import jdk.jfr.Description;
 
 /**
  *
  * @author rjbea
  */
 public class Event {
-    private String EventName;
-    private java.time.LocalDate Day;
-    private java.time.LocalTime StartTime;
-    private java.time.Duration Duration;
-    private java.time.LocalTime EndTime;
-    private String Summary;
-    private String Description;
-    private String Location;
+    protected String EventName;
+    protected java.time.LocalDate Day;
+    protected java.time.LocalTime StartTime;
+    protected java.time.Duration Duration;
+    protected java.time.LocalTime EndTime;
+    protected String Summary;
+    protected String Description;
+    protected String Location;
     private String ID;
-    private cms.EventStatus EventStatus;
+    protected cms.EventStatus EventStatus;
     
-    public Event(String Name, int Day, int Month, int Year, int StartHour, 
-            int StartMinute, long DurationHour){
+    
+    /**
+     * 
+     * @param ID
+     * @param Name
+     * @param Day
+     * @param Month
+     * @param Year
+     * @param StartHour
+     * @param StartMinute
+     * @param EndHour
+     * @param EndMinute
+     * @param Summary
+     * @param Description
+     * @param Location 
+     */
+    public Event(String ID, String Name, int Day, int Month, int Year, int StartHour, 
+            int StartMinute, int EndHour, int EndMinute, String Summary, String Description, String Location){
         
         // TODO: Validate
         java.time.LocalDate today = java.time.LocalDate.now();
         
+        this.ID = ID;
         this.EventName = Name;
-        this.Day = java.time.LocalDate.of(Year, Month, Month);
+        this.Day = java.time.LocalDate.of(Year, Month, Day);
         this.StartTime = java.time.LocalTime.of(StartHour, StartMinute);
-        this.Duration = java.time.Duration.ofHours(DurationHour);
-        this.EndTime = this.StartTime.plusHours(DurationHour);
-        
+        this.EndTime = java.time.LocalTime.of(EndHour, EndMinute);
+        this.Description = Description;
+        this.Location = Location;
+        this.Summary = Summary;
         
         if(this.Day.isAfter(today) == true){
             this.setEventStatus("Upcoming");
@@ -43,49 +63,102 @@ public class Event {
         
     }
     
+    /**
+     * 
+     * @param ID
+     * @param Name
+     * @param Day
+     * @param Month
+     * @param Year
+     * @param StartHour
+     * @param StartMinute
+     * @param EndHour
+     * @param EndMinute
+     * @param Summary
+     * @param Description
+     * @param Location
+     * @param EventStatus 
+     */
+    public Event(String ID, String Name, int Day, int Month, int Year, 
+            int StartHour, int StartMinute, int EndHour, int EndMinute, 
+            String Summary, String Description, String Location, String EventStatus){
+        
+        this.ID = ID;
+        this.EventName = Name;
+        this.Day = java.time.LocalDate.of(Year, Month, Day);
+        this.StartTime = java.time.LocalTime.of(StartHour, StartMinute);
+        this.EndTime = java.time.LocalTime.of(EndHour, EndMinute);
+        this.Summary = Summary;
+        this.Description = Description;
+        this.Location = Location;
+        this.setEventStatus(EventStatus);
+        
+    }
+
+    /**
+     * 
+     * @param status
+     * @return 
+     */    
     public boolean setEventStatus(String status){
         boolean results = false;
         java.time.LocalDate today = java.time.LocalDate.now();
         if(status.equalsIgnoreCase("Upcoming")){
-            if(this.Day.isAfter(today) ==false){
+            if(this.getDay().isAfter(today) ==false){
                 System.out.println("Error: Cannot make events before current date 'Upcoming'");
             }else{
-                this.EventStatus = EventStatus.Upcoming;
+                this.setEventStatus(getEventStatus().Upcoming);
                 results = true;
             }
         }else if(status.equalsIgnoreCase("Past")){
-            this.EventStatus = EventStatus.Past;
+            this.setEventStatus(getEventStatus().Past);
             results = true;
         } else if(status.equalsIgnoreCase("Canceled")){
-            this.EventStatus = EventStatus.Cancled;
+            this.setEventStatus(getEventStatus().Canceled);
             results = true;
         }
         return results;
     }
     
+    /**
+ * 
+ * @param name
+ * @return 
+ */    
     public boolean setName(String name){
         boolean results = false;
         if((name == null)|| (name.length() <= 0)){
             // Do nothing. Return False
         } else{
-            this.EventName = name;
+            this.setEventName(name);
             results = true;
         }
         return results;
     }
-    
+
+    /**
+ * 
+ * @return 
+ */    
     public boolean Cancel(){
-        this.EventStatus = EventStatus.Cancled;
+        this.setEventStatus(getEventStatus().Canceled);
         return true;
     }
-    
+
+    /**
+ * 
+ * @param Day
+ * @param Month
+ * @param Year
+ * @return 
+ */    
     public boolean setDate(int Day, int Month, int Year){
         boolean results = false;
         java.time.LocalDate today = java.time.LocalDate.now();
         
-        this.Day = java.time.LocalDate.of(Year, Month, Month);
-        
-        if(this.Day.isAfter(today) == true){
+        this.setDay(java.time.LocalDate.of(Year, Month, Day));
+        results = true;
+        if(this.getDay().isAfter(today) == true){
             this.setEventStatus("Upcoming");
         } else {
             this.setEventStatus("Past");
@@ -94,20 +167,36 @@ public class Event {
         return results;
     }
 
+    /**
+ * 
+ * @return 
+ */    
+    public String getID(){
+        return this.ID;
+    }
+
+    /**
+     * 
+     * @return 
+     */
     public String toString(){
         String results = "";
-        results += "BEGIN:"+this.EventName+'\n';
-        results += "DTSTART:"+this.Day+"t"+this.StartTime+'\n';
-        results += "DTEND:"+this.Day+"t"+this.EndTime+'\n';
-        results += "SUMMARY:"+this.Summary+'\n';
-        results += "DESCRIPTION:"+this.Description+'\n';
-        results += "LOCATION:"+this.Location+'\n';
+        results += "BEGIN:"+this.getEventName()+'\n';
+        results += "DTSTART:"+this.getDay()+"t"+this.getStartTime()+'\n';
+        results += "DTEND:"+this.getDay()+"t"+this.getEndTime()+'\n';
+        results += "SUMMARY:"+this.getSummary()+'\n';
+        results += "DESCRIPTION:"+this.getDescription()+'\n';
+        results += "LOCATION:"+this.getLocation()+'\n';
         results += "UID:"+this.ID+'\n';
-        results += "END:"+this.EventName+'\n';
+        results += "END:"+this.getEventName()+'\n';
         
         return results;
     }
     
+    /**
+     * 
+     * @return 
+     */
     public String toICS(){
         String results = "BEGIN:VCALENDAR" +'\n';
         results+= "VERSION:2.0" +'\n';
@@ -118,13 +207,203 @@ public class Event {
         
     }
     
+    /**
+     * 
+     * @param event
+     * @return 
+     */
+    public static Event fromICS(String event) {
+        String eventName = extractValue(event, "BEGIN:");
+        String startDate = extractValue(event, "DTSTART:");
+        String endDate = extractValue(event, "DTEND:");
+        String summary = extractValue(event, "SUMMARY:");
+        String description = extractValue(event, "DESCRIPTION:");
+        String location = extractValue(event, "LOCATION:");
+        String id = extractValue(event, "UID:");
+
+        String[] start = startDate.split("t");
+        String[] sDate = extractDate(start[0]);
+        
+        String[] sTime = extractTime(start[1]);
+        String[] end = endDate.split("t");
+        String[] eTime = extractTime(end[1]);
+        cms.Event current;
+        current = new cms.Event(id, eventName, java.lang.Integer.parseInt(sDate[2]), 
+                java.lang.Integer.parseInt(sDate[1]) , java.lang.Integer.parseInt(sDate[0]), 
+                java.lang.Integer.parseInt(sTime[0]), java.lang.Integer.parseInt(sTime[1]), 
+                java.lang.Integer.parseInt(eTime[0]), java.lang.Integer.parseInt(eTime[1]), 
+                summary, description, location);
+        
+        
+        return current;
+    }
+    
+    /**
+     * 
+     * @param localString
+     * @return 
+     */
+    private static String[] extractDate(String localString){
+        return localString.split("-");
+    }
+    
+    /**
+     * 
+     * @param localString
+     * @return 
+     */
+    private static String[] extractTime(String localString){
+        return localString.split(":");
+    }
+
+    /**
+     * 
+     * @param event
+     * @param propertyName
+     * @return 
+     */
+    private static String extractValue(String event, String propertyName) {
+        String[] lines = event.split("\\r?\\n");
+        for (String line : lines) {
+            if (line.startsWith(propertyName)) {
+                return line.substring(propertyName.length());
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @return the EventName
+     */
+    public String getEventName() {
+        return EventName;
+    }
+
+    /**
+     * @param EventName the EventName to set
+     */
+    public void setEventName(String EventName) {
+        this.EventName = EventName;
+    }
+
+    /**
+     * @return the Day
+     */
+    public java.time.LocalDate getDay() {
+        return Day;
+    }
+
+    /**
+     * @param Day the Day to set
+     */
+    public void setDay(java.time.LocalDate Day) {
+        this.Day = Day;
+    }
+
+    /**
+     * @return the StartTime
+     */
+    public java.time.LocalTime getStartTime() {
+        return StartTime;
+    }
+
+    /**
+     * @param StartTime the StartTime to set
+     */
+    public void setStartTime(java.time.LocalTime StartTime) {
+        this.StartTime = StartTime;
+    }
+
+    /**
+     * @return the Duration
+     */
+    public java.time.Duration getDuration() {
+        return Duration;
+    }
+
+    /**
+     * @param Duration the Duration to set
+     */
+    public void setDuration(java.time.Duration Duration) {
+        this.Duration = Duration;
+    }
+
+    /**
+     * @return the EndTime
+     */
+    public java.time.LocalTime getEndTime() {
+        return EndTime;
+    }
+
+    /**
+     * @param EndTime the EndTime to set
+     */
+    public void setEndTime(java.time.LocalTime EndTime) {
+        this.EndTime = EndTime;
+    }
+
+    /**
+     * @return the Summary
+     */
+    public String getSummary() {
+        return Summary;
+    }
+
+    /**
+     * @param Summary the Summary to set
+     */
+    public void setSummary(String Summary) {
+        this.Summary = Summary;
+    }
+
+    /**
+     * @return the Description
+     */
+    public String getDescription() {
+        return Description;
+    }
+
+    /**
+     * @param Description the Description to set
+     */
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    /**
+     * @return the Location
+     */
+    public String getLocation() {
+        return Location;
+    }
+
+    /**
+     * @param Location the Location to set
+     */
+    public void setLocation(String Location) {
+        this.Location = Location;
+    }
+
+    /**
+     * @return the EventStatus
+     */
+    public cms.EventStatus getEventStatus() {
+        return EventStatus;
+    }
+
+    /**
+     * @param EventStatus the EventStatus to set
+     */
+    public void setEventStatus(cms.EventStatus EventStatus) {
+        this.EventStatus = EventStatus;
+    }
 
 }
 
-    enum EventStatus{
+enum EventStatus{
     
     Upcoming,
     Past,
-    Cancled;
+    Canceled;
     
 }
