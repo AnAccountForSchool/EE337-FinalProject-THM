2ab37988b219066dc3bc1714d42955697c8287d7
AnAccountForSchool <166833777+AnAccountForSchool@users.noreply.github.com>
2024-04-23 14:04:55 -0500

    Update CMSManager.java

--- CMSManager.java
+++ CMSManager.java
@@ -38,7 +38,7 @@ public class CMSManager {
         if(args.length > 0){
             CommandLineInterface(args); // this works to create a database if there is none
         } else{
-            Help(); // currently there is no GUI Interface, comment out when there is
+            GUIInterface(args); // currently there is no GUI Interface, comment out when there is
         }
     }
     
@@ -46,10 +46,10 @@ public class CMSManager {
     /**
      *  All GUI Creation will start here
      */
-    public static void GUIInterface(){
-        java.util.ArrayList<cms.ContactInfo> ContactInfos = new java.util.ArrayList<>();
-        java.util.ArrayList<cms.Event> Events = new java.util.ArrayList<>();
-        java.util.ArrayList<cms.NewsLetter> NewsLetters = new java.util.ArrayList<>();
+    public static void GUIInterface(String[] args){
+
+        
+        cms.ContactManagerGUI.main(args);
     
     }
     
@@ -58,72 +58,9 @@ public class CMSManager {
      * @param args 
      */
     public static void CommandLineInterface(String[] args){
-        System.out.println("Here");
-        java.util.ArrayList<cms.ContactInfo> ContactInfos = new java.util.ArrayList<>();
-        java.util.ArrayList<cms.Event> Events = new java.util.ArrayList<>();
-        java.util.ArrayList<cms.NewsLetter> NewsLetters = new java.util.ArrayList<>();
         
-        // args information for debugging
-        // need to comment out for release
-        int index;     
-        System.out.println("Count: "+ args.length);
-        for(index = 0; index<args.length; index++){
-            System.out.println(" args["+index+"] = " + args[index]);
-        }
+        cms.CommandLineManager.CommandLineManagerStart(args);
         
-        if(args[0].equalsIgnoreCase("/Initialize")==true){
-            System.out.println("Initializing");
-            betaOutput();
-        }
-        
-        ContactInfos = cms.CMSManager.importFromDatabase(ContactInfos);
-        
-        if((args.length == 6) && (args[0].equalsIgnoreCase("/add"))) {            
-            addToArrayList(ContactInfos, args[1], args[2], args[3], args[4], args[5], "", "", "", "", "", "");
-        } else if((args.length == 12) && (args[0].equalsIgnoreCase("/add"))){
-            addToArrayList(ContactInfos, args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11]);                
-        }else if((args.length == 3) && (args[0].equalsIgnoreCase("/search"))) {            
-            java.util.ArrayList<cms.ContactInfo> SearchResults = new java.util.ArrayList<>();
-            SearchResults = Search(ContactInfos, args[1], args[2]);
-            if(SearchResults.size()>0){
-                String FileName = "SearchResults_" + args[1]+"_"+args[2]+".txt";
-                Export(SearchResults, FileName , "custom");
-            } else{
-                System.out.println("No Results Found");
-            }
-        } else if((args.length == 4) && (args[0].equalsIgnoreCase("/update"))) {
-            Update(ContactInfos, args[1], args[2], args[3]);
-            
-        } else if((args.length == 2) && (args[0].equalsIgnoreCase("/delete"))) {
-            Delete(ContactInfos, args[1]);
-        } else if((args.length == 2) && (args[0].equalsIgnoreCase("/archive"))) {
-            Update(ContactInfos, args[1], "ContactStatus", "Archive");
-        } else if((args.length == 1) && (args[0].equalsIgnoreCase("/validate"))) {
-            // this might come later
-        } else if ((args.length == 13) && (args[0].equalsIgnoreCase("/EventAdd")==true)){
-            // 1 = id 2 = name 3 = day 4 = month 5 = year 6 = Hr1 7 = min1 8 = hr 2 9 = min2 10 = Sum 11 = desc 12 = loc
-            EventAdd(args[1], args[2], java.lang.Integer.parseInt(args[6]), 
-                    java.lang.Integer.parseInt(args[5]), java.lang.Integer.parseInt(args[4]), 
-                    java.lang.Integer.parseInt(args[6]), java.lang.Integer.parseInt(args[7]), 
-                    java.lang.Integer.parseInt(args[8]), java.lang.Integer.parseInt(args[9]), 
-                    args[12], args[11], args[12]);
-        }else if((args.length == 3)&&(args[0].equalsIgnoreCase("/EventSend"))){
-            
-            java.util.ArrayList<cms.ContactInfo> ResultContacts = cms.CMSManager.Search(ContactInfos, args[2], args[3]);
-            cms.Event Event = getEvent(args[1]);
-            if(Event != null){
-                cms.CMSManager.EventSend(Event, ContactInfos);
-            } else System.out.println("Event not found");
-        
-        }else if(args[0].equalsIgnoreCase("/beta")== true){
-            //betaOutput();
-            //betaEvent();
-            betaNewsletter();
-        }else{
-            // Display help message
-            Help();
-        }
-    
     }
     
     /**
@@ -634,7 +571,7 @@ public class CMSManager {
         
         java.util.ArrayList<cms.ContactInfo> Contacts = new java.util.ArrayList<>();
         Contacts = importFromDatabase(Contacts);
-        
+        System.out.println(Contacts.get(0).toXML());
         java.util.ArrayList<cms.NewsLetter> NewsLetters = NewsLetter.load(Contacts);
         
         for(cms.NewsLetter Current : NewsLetters){
@@ -645,6 +582,17 @@ public class CMSManager {
         
         
         return false;
+    }
+    
+    public static cms.NewsLetter NewsLetterSearch(String ID){
+        java.util.ArrayList<cms.NewsLetter> NewsLetters = new java.util.ArrayList<>();
+        cms.NewsLetter results;
+        for(cms.NewsLetter current: NewsLetters){
+            if(current.ID.equalsIgnoreCase(ContRepForm)==true ){
+                return current;
+            }
+        }
+        return null;
     }
     
     /**
@@ -810,14 +758,14 @@ public class CMSManager {
         System.out.println(Text);
                 
     }
-   /**
+    /**
     * 
     * @param Contacts 
     */ 
     public static void beta(java.util.ArrayList<cms.ContactInfo> Contacts){
         betaOutput();
     }
-/**
+    /**
  * 
  * @param Contacts 
  */
@@ -850,7 +798,12 @@ public class CMSManager {
         
         BetaList = NewsLetterImport(NewsletterRepository);
         
-        System.out.println(BetaList.get(0).toCustom());
+        System.out.println(beta.toCustom());
+        
+        java.util.ArrayList<cms.ContactInfo> Contacts = new java.util.ArrayList<>();
+        
+
+        NewsLetterSend(beta);
         
     }
     /**
